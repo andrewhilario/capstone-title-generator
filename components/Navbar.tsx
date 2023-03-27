3ddf65067b1e589c2f614a5f07282e280313666c
Drew Hilario <hilarioandrew12@gmail.com>
2023-03-27 16:40:13 +0800

    Added the repo link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,9 +11,11 @@ const Navbar = () => {
             <h2 className='font-medium'>Title Generator</h2>
           </div>
         </Link>
-        <div className='border-2 px-4 py-2 rounded-2xl border-slate-500 text-md text-slate-500 font-semibold text-center transition ease-in hover:bg-slate-500 hover:text-white cursor-pointer'>
-          Repo
-        </div>
+        <Link href='https://github.com/andrewhilario/capstone-title-generator'>
+          <div className='border-2 px-4 py-2 rounded-2xl border-slate-500 text-md text-slate-500 font-semibold text-center transition ease-in hover:bg-slate-500 hover:text-white cursor-pointer'>
+            Repo
+          </div>
+        </Link>
       </nav>
     </>
   );
